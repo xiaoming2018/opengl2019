2e0aa24fa245b2b05cadca630a99e5ec72adf79a
xiaoming <1209819655@qq.com>
2019-08-11 21:28:37 +0800

    perspective

--- GLFWDEMO/LearnOpenGL.cpp
+++ GLFWDEMO/LearnOpenGL.cpp
@@ -19,11 +19,54 @@ float verticesNew[] = {
 };
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f, 1, 0, 0,		//左下
-	-0.5f, 0.5f, 0.0f, 0, 1, 0,		//左上
-	0.5f, 0.5f, 0.0f, 0, 0, 1,      //右上
-	0.5f, -0.5f, 0.0f, 1, 0, 0       //右下  
+	 -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+	  0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+	  0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+	  0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+	 -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+	 -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 
+	 -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+	  0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+	  0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+	  0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+	 -0.5f,  0.5f,  0.5f, 0.0f, 1.0f,
+	 -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+	 -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+	 -0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+	 -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+	 -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+	 -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+	 -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+
+	  0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+	  0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+	  0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+	  0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+	  0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+	  0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+
+	 -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+	  0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+	  0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+	  0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+	 -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+	 -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+	 -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+	  0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+	  0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+	  0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+	 -0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+	 -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+};
+
+glm::vec3 cubePositon[] =
+{
+	glm::vec3(0.0f, 0.0f,  0.0f),
+	glm::vec3(2.0f, 5.0f, -15.0f),
+	glm::vec3(-2.0f, 3.0f, -7.5f)
 };
 
 // EBO 索引数组
@@ -34,7 +77,7 @@ unsigned int indices[] = {
 
 void init();
 void VAOSet();
-
+void texture(Shader &myShader);
 
 int main()
 {
@@ -45,67 +88,46 @@ int main()
 	//vec = trans * vec;
 	//cout << " x: " <<vec.x << vec.y << vec.z << endl;
 
-	glm::mat4 trans = glm::mat4(1.0f);
-	trans = glm::rotate(trans, glm::radians(90.0f),glm::vec3(0.0f,0.0f,1.0f)); // 度数 和 参考轴
-	trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
+	//glm::mat4 trans = glm::mat4(1.0f);
+	//trans = glm::rotate(trans, glm::radians(90.0f),glm::vec3(0.0f,0.0f,1.0f)); // 度数 和 参考轴
+	//trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
 
 	// 初始化
 	init();
 	VAOSet();
 
 	Shader myShader;
-	myShader.userShader();
-	glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "transform"),1,GL_FALSE,glm::value_ptr(trans));
+	texture(myShader);
 
-	int width, height, nrchannels;
-	unsigned char * data = NULL;
-	stbi_set_flip_vertically_on_load(true);  // 实现y轴的反转
+	glEnable(GL_DEPTH_TEST); // 深度
 
-	unsigned int texture[2];
-	glGenTextures(2, texture);
-
-	// 加载第一个纹理
-	glBindTexture(GL_TEXTURE_2D, texture[0]);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR);// 纹理放大时 使用 最近原则过滤
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST); // 纹理缩小时 使用 最近原则过滤
-	data = stbi_load("1.jpg", &width, &height, &nrchannels, 0);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(data);
-
-	// 加载第二个纹理 纹理图片太小 会加载错误
-	glBindTexture(GL_TEXTURE_2D, texture[1]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);// 纹理放大时 使用 最近原则过滤
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // 纹理缩小时 使用 最近原则过滤
-	data = stbi_load("temp.jpg", &width, &height, &nrchannels, 0);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-	// glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data); // png 格式  带有透明度参数
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(data);
-
-	myShader.userShader();
-	glUniform1i(glGetUniformLocation(myShader.ID, "ourTexture1"), 0);
-	glUniform1i(glGetUniformLocation(myShader.ID, "ourTexture2"), 1);
-
-	glActiveTexture(GL_TEXTURE0); // 先激活再绑定 纹理单元0 默认是激活的
-	glBindTexture(GL_TEXTURE_2D, texture[0]);
-
-	glActiveTexture(GL_TEXTURE1); // 纹理单元
-	glBindTexture(GL_TEXTURE_2D, texture[1]);
-
-
-	// 绘制方式 用line方式 默认是gl_file
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	// 渲染引擎
 	while (!glfwWindowShouldClose(windows))
 	{
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 设置窗口背景颜色
-		glClear(GL_COLOR_BUFFER_BIT);  //  填充颜色
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  //  填充颜色
 
-		
-		//glUseProgram(shaderProgram);   // 使用自定义渲染程序
-		//glDrawArrays(GL_TRIANGLES, 0, 3); // 读取三个点，进行绘制 画三角
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+		for (size_t i = 0; i < 3; i++)
+		{
+			glm::mat4 trans = glm::mat4(1.0f);
+			glm::mat4 model = glm::mat4(1.0f);  // 模型矩阵
+			model = glm::translate(trans, cubePositon[i]); //  平移
+			//model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // 绕x轴旋转 45°
+			model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));   // 随时间旋转
+
+			glm::mat4 view = glm::mat4(1.0f); // 需要初始化
+			view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+			glm::mat4 projection = glm::mat4(1.0f);
+			projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+			trans = projection * view * model;
+
+			myShader.userShader();
+			glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36); // 读取三个点，进行绘制 画三角
+		}
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 
 		glfwPollEvents(); // 接受事件
 		glfwSwapBuffers(windows);
@@ -129,13 +151,13 @@ void VAOSet()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesNew), verticesNew, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	// 告诉 着色器 VBO的结构 - 位置属性 0：VAO的第一个位置 ；3：（xyz）； 值类型： float；  是否标准化 ； 大小-步长； 偏移VBO的位置-- VBO中在前，偏移量为0 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	// 启用 VAO 的第一个位置
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) (3 * sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	// 启用 VAO 的第一个位置
 	glEnableVertexAttribArray(1);
 
@@ -179,4 +201,44 @@ void init()
 		cout << "Failed to initialize GLAD" << endl;
 		return;
 	}
+}
+
+void texture(Shader &myShader)
+{
+	int width, height, nrchannels;
+	unsigned char * data = NULL;
+	stbi_set_flip_vertically_on_load(true);  // 实现y轴的反转
+
+	unsigned int texture[2];
+	glGenTextures(2, texture);
+
+	// 加载第一个纹理
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);// 纹理放大时 使用 最近原则过滤
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // 纹理缩小时 使用 最近原则过滤
+	data = stbi_load("1.jpg", &width, &height, &nrchannels, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(data);
+
+	// 加载第二个纹理 纹理图片太小 会加载错误
+	glBindTexture(GL_TEXTURE_2D, texture[1]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);// 纹理放大时 使用 最近原则过滤
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // 纹理缩小时 使用 最近原则过滤
+	data = stbi_load("temp.jpg", &width, &height, &nrchannels, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	// glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data); // png 格式  带有透明度参数
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(data);
+
+
+	myShader.userShader();
+	glUniform1i(glGetUniformLocation(myShader.ID, "ourTexture1"), 0);
+	glUniform1i(glGetUniformLocation(myShader.ID, "ourTexture2"), 1);
+
+	glActiveTexture(GL_TEXTURE0); // 先激活再绑定 纹理单元0 默认是激活的
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+
+	glActiveTexture(GL_TEXTURE1); // 纹理单元
+	glBindTexture(GL_TEXTURE_2D, texture[1]);
 }
