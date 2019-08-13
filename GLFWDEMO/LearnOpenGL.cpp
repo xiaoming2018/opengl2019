22541536a26ac1f650b7ab10b95b85788edab788
xiaoming <1209819655@qq.com>
2019-08-13 15:19:59 +0800

    11

--- GLFWDEMO/LearnOpenGL.cpp
+++ GLFWDEMO/LearnOpenGL.cpp
@@ -83,17 +83,6 @@ float currentFrameTime = glfwGetTime();
 
 int main()
 {
-	//// 列向量 表示
-	//glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-	//glm::mat4 trans = glm::mat4(1.0f);
-	//trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
-	//vec = trans * vec;
-	//cout << " x: " <<vec.x << vec.y << vec.z << endl;
-
-	//glm::mat4 trans = glm::mat4(1.0f);
-	//trans = glm::rotate(trans, glm::radians(90.0f),glm::vec3(0.0f,0.0f,1.0f)); // 度数 和 参考轴
-	//trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
-
 	// 初始化
 	GLFWwindow* windows = init();
 	VAOSet();
@@ -170,44 +159,31 @@ GLFWwindow* init()
 
 void VAOSet()
 {
-	unsigned int VBO[2], VAO, EBO; // ID 号
+	unsigned int VBO, VAO, EBO; // ID 号
 
 	// 生成 VAO 
 	glGenVertexArrays(1, &VAO);
 	// 绑定 VAO
 	glBindVertexArray(VAO);
 
-	glGenBuffers(2, VBO); // 显存空间
-	glGenBuffers(1, &EBO); // 显存空间
+	glGenBuffers(1, &VBO); // 显存空间
+	//glGenBuffers(1, &EBO); // 显存空间
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
+	// 绑定VBO数据
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	// 告诉 着色器 VBO的结构 - 位置属性 0：VAO的第一个位置 ；3：（xyz）； 值类型： float；  是否标准化 ； 大小-步长； 偏移VBO的位置-- VBO中在前，偏移量为0 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	// 启用 VAO 的第0个位置
 	glEnableVertexAttribArray(0);
 
+	// 纹理坐标
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	// 启用 VAO 的第1个位置
 	glEnableVertexAttribArray(1);
-
-	//glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-	//// 数据存放
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	//// 告诉 着色器 VBO的结构 - 位置属性  1：VAO的第二个位置 ；3：（xyz）； 值类型： float；  是否标准化 ； 大小-步长； 偏移VBO的位置-- VBO中在前，偏移量为0 
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	//// 启用 VAO 的第一个位置
-	//glEnableVertexAttribArray(1);
-
-	//// 告诉 着色器 VBO的结构 - 颜色属性  2：VAO的第三个位置   3：（xyz）； 值类型：  float；   是否标准化 ；  大小-步长； 偏移--在VBO中在后，偏移量为3个float大小
-	//glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	//// 启用 VAO 的第二个位置
-	//glEnableVertexAttribArray(2);
 }
 
 void texture(Shader *myShader)
@@ -237,7 +213,6 @@ void texture(Shader *myShader)
 	// glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data); // png 格式  带有透明度参数
 	glGenerateMipmap(GL_TEXTURE_2D);
 	stbi_image_free(data);
-
 
 	myShader->userShader();
 	glUniform1i(glGetUniformLocation(myShader->ID, "ourTexture1"), 0);
